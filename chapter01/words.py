74d059276a9636bd64355f273d46a88d716ef8de
AK <andrei.avk@gmail.com>
2012-10-10 02:26:13 -0400

    updated words

--- chapter01/words.py
+++ chapter01/words.py
@@ -3,18 +3,15 @@
 # Imports {{{
 from __future__ import print_function, unicode_literals, division
 
-import os, sys
-from random import choice, randint
-from string import join, uppercase, letters
-from os.path import expanduser, exists
+import os
+from random import choice
+from string import letters
 
-from term import Term
-
-maxx, maxy   = 2, 2
+num_words    = 6
 initial_hide = 0.7
 start_points = 30
+hiddenltr    = '_'
 wordsfn      = "words"
-datafn       = expanduser("~/.words_data")
 # }}}
 
 
@@ -27,11 +24,11 @@ class Word(object):
     def __len__(self):
         return len(self.word)
 
-    def guess(self, i, l):
+    def guess(self, i, letter):
         """Reveal all instances of `l` if word[i] == `l` & reveal random letter in one other word."""
-        if self.word[i] == l:
-            self.reveal_letter(l)
-            lst = [w for w in board if len(w.hidden)>1 and w != self]
+        if i in self.hidden and self.word[i] == letter:
+            self.reveal(letter)
+            lst = [w for w in words if len(w.hidden)>1 and w != self]
             if lst:
                 choice(lst).revealrnd()
             return True
@@ -57,101 +54,30 @@ class Word(object):
         """Hide letters according to `hidden`, e.g. if 0.7, hide 70%."""
         length      = len(self)
         num_to_hide = round(length * hidden)
-        self.hidden = range(length)
         while len(self.hidden) < num_to_hide:
             self.hide( choice(range(length)) )
 
     def __str__(self):
-        return ''join( (hiddenltr if n in self.hidden else l) for n, l in enumerate(self.word) )
+        word = ((hiddenltr if n in self.hidden else l) for n, l in enumerate(self.word))
+        return ' '.join(word)
 
 
-class Command(object):
-    term = Term()
-
-    movekeys = {
-        '[D' : "left",
-        '[C' : "right",
-        '[B' : "down",
-        '[A' : "up",
-
-        'h'  : "left",
-        'l'  : "right",
-        'j'  : "down",
-        'k'  : "up",
-    }
-
-    cmds = dict(
-        q = "quit",
-        r = "random",
-        b = "back",
-        h = "help",
-    )
-
-    def help(self):
-        print "move: left/right/down/up   Enter: open word / guess letter\n" \
-              "r: reveal random letter   b: back to wordlist   q: quit"
-
-    def quit(self):
-        if self.term.getch("quit? [y/n] ") == 'y':
-            sys.exit()
-
-    def guess(self):
-        """Let player guess current letter."""
-        if char.listmode: return
-        l = self.term.getch("letter? ")
-        if board.current().reveal(char.i, l):
-            char.points += 1
-        else:
-            char.points -= 1
-
-    def random(self):
-        if char.points >= 3:
-            board.current().revealrnd()
-            char.points -= 3
-
-    def open(self):
-        """Open current word or guess current letter."""
-        if char.listmode:
-            char.listmode = False
-            char.i = 0
-        else:
-            self.guess()
-
-    def back(self):
-        char.listmode = True
-
-    def blank(self):
-        pass
-
-    def uinput(self):
-        cmd = self.term.getch('> ')
-        if cmd in self.movekeys:
-            return char.move(self.movekeys[cmd])
-        elif cmd == '\n':
-            self.open()
-        else:
-            getattr(self, self.cmds.get(cmd, "blank"))()
-
+def display():
+    print( '\n'.join(str(word) for word in words) ); print()
 
 def main():
-    while 1:
-        board.display() if char.listmode else board.show_word()
+    display()
+    words[0].revealrnd()
+    display()
 
-        if   board.all_done(): won()
-        elif char.points < 0: lost()
-        cmd.uinput()
-
-def save_top_score(self):
-    data = shelve.open(datafn)
-    score = data.get("score", 0)
-    if self.points > score:
-        print("You have the top score!!\n")
-        data["score"] = self.points
-    data.close()
+    word = words[2]
+    word.guess(0, 's')
+    display()
+    word.guess(1, 'n')
+    display()
 
 
 if __name__ == "__main__":
-    words = open(wordsfn).readlines()
-    char  = Char()
-    board = Board()
+    wordlist = open(wordsfn).readlines()
+    words    = [ Word(word) for word in "sunny clouds snowflake".split()]
     main()
