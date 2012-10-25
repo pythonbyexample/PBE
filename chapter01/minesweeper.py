615f57dc479f55bd5cb2fc52a19953034b9bf707
AK <andrei.avk@gmail.com>
2012-10-24 23:15:37 -0400

    refactored minesweeper into two files

--- chapter01/minesweeper.py
+++ chapter01/minesweeper.py
@@ -3,146 +3,15 @@
 from __future__ import print_function, unicode_literals, division
 
 import sys
-from random import choice, randint
-from time import time, sleep
+from random import randint
+from time import sleep
 
-from utils import Loc, joins
+from utils import Loc
+from minesweeper_lib import Board, Minesweeper
 
-size       = 8
-num_mines  = randint(4, 8)
-num_mines = 2
-ai_run     = 0
-
-space      = ' '
-blank      = ' '
-hiddenchar = '.'
-minechar   = '*'
-divider    = '-' * (size * 3 + 5)
-
-
-class Tile(object):
-    hidden = True
-    mine   = False
-    marked = False
-    number = 0
-
-    def __init__(self, x, y):
-        self.loc = Loc(x,y)
-
-    def __repr__(self):
-        if self.marked   : s = minechar
-        elif self.hidden : s = hiddenchar
-        elif self.mine   : s = minechar
-        else             : s = str(self.number or blank)
-        return "%2s" % s
-
-    def toggle_mark(self):
-        """Toggle 'mine' mark on/off."""
-        self.marked = not self.marked
-        self.hidden = not self.hidden
-
-
-class Board(object):
-    """Minesweeper playing board."""
-    def __init__(self, size):
-        self.size = size
-        self.board = [ [Tile(x,y) for x in range(size)] for y in range(size) ]
-        for _ in range(num_mines):
-            self.random_empty().mine = True
-
-        for tile in self:
-            tile.number = sum( ntile.mine for ntile in self.neighbours(tile) )
-
-    def __getitem__(self, loc):
-        return self.board[loc.y][loc.x]
-
-    def __iter__(self):
-        """Iterate over board tile locations."""
-        return ( self[Loc(x,y)] for x in range(self.size) for y in range(self.size) )
-
-    def marked_or_revealed(self, tile):
-        return bool(not tile.hidden or tile.mine and tile.marked)
-
-    def cleared(self):
-        """All mines defused?"""
-        return all( self.marked_or_revealed(tile) for tile in self )
-
-    def all_hidden(self):
-        return [tile for tile in self if tile.hidden]
-
-    def random_hidden(self):
-        return choice(self.all_hidden())
-
-    def all_empty(self):
-        return [tile for tile in self if not tile.mine]
-
-    def random_empty(self):
-        return choice(self.all_empty())
-
-
-    def draw(self):
-        sp2 = space*2
-        print(space*4, sp2.join( [str(n+1) for n in range(self.size)] ))
-        print('\n')
-
-        for n, row in enumerate(self.board):
-            print(n+1, space, joins(row, space))
-            print()
-        print(divider)
-
-    def reveal(self, tile):
-        if not tile.number:
-            self.reveal_empty_neighbours(tile)
-        tile.hidden = False
-        return tile
-
-    def reveal_empty_neighbours(self, tile):
-        """ Reveal all empty (number=0) tiles adjacent to starting tile `loc` and subsequent unhidden tiles.
-            Uses floodfill algorithm.
-        """
-        if tile.number != 0:
-            tile.hidden = False
-        if not tile.hidden:
-            return
-
-        tile.hidden = False
-        for ntile in self.neighbours(tile):
-            self.reveal_empty_neighbours(ntile)
-
-    def neighbours(self, tile):
-        """Return the list of neighbours of `loc`."""
-        x, y = tile.loc
-        coords = (-1,0,1)
-        locs = set((x+n, y+m) for n in coords for m in coords) - set( [(x,y)] )
-        return [ self[ Loc(*tup) ] for tup in locs if self.valid(*tup) ]
-
-    def valid(self, x, y):
-        return bool( x+1 <= self.size and y+1 <= self.size and x >= 0 and y >= 0 )
-
-
-class Minesweeper(object):
-    start    = time()
-    win_msg  = "All mines cleared (%d:%d)!"
-    lose_msg = "KABOOM. END."
-
-    def check_end(self, tile):
-        """Check if game is lost (stepped on a mine), or won (all mines found)."""
-        if tile.mine and not tile.marked:
-            self.game_lost()
-        elif board.cleared():
-            self.game_won()
-
-    def game_lost(self):
-        for tile in board.all_hidden():
-            board.reveal(tile)
-        board.draw()
-        print(self.lose_msg)
-        sys.exit()
-
-    def game_won(self):
-        elapsed = time() - self.start
-        print(self.win_msg % (elapsed/60, elapsed%60))
-        sys.exit()
+size      = 8
+num_mines = randint(4, 8)
+ai_run    = 0
 
 
 class Test(object):
@@ -161,7 +30,7 @@ class Test(object):
                     pass
 
     def manual_move(self):
-        """Mark mine if """
+        """Get user command and mark mine or reveal a location; check if game is won/lost."""
         inp = raw_input(self.prompt)
         if inp == 'q': sys.exit()
 
@@ -186,6 +55,6 @@ class Test(object):
 
 
 if __name__ == "__main__":
-    board = Board(size)
-    msweep = Minesweeper()
+    board = Board(size, num_mines)
+    msweep = Minesweeper(board)
     Test().test()
---
+++ chapter01/minesweeper_lib.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+
+from __future__ import print_function, unicode_literals, division
+
+import sys
+from random import choice as rndchoice
+from random import randint
+from time import time
+
+from utils import Loc, joins
+
+space      = ' '
+blank      = ' '
+hiddenchar = '.'
+minechar   = '*'
+
+
+class Tile(object):
+    hidden = True
+    mine   = False
+    marked = False
+    number = 0
+
+    def __init__(self, x, y):
+        self.loc = Loc(x,y)
+
+    def __repr__(self):
+        if self.marked   : s = minechar
+        elif self.hidden : s = hiddenchar
+        elif self.mine   : s = minechar
+        else             : s = str(self.number or blank)
+        return "%2s" % s
+
+    def toggle_mark(self):
+        """Toggle 'mine' mark on/off."""
+        self.marked = not self.marked
+        self.hidden = not self.hidden
+
+
+class Board(object):
+    """Minesweeper playing board."""
+    def __init__(self, size, num_mines):
+        self.size    = size
+        self.divider = '-' * (size * 3 + 5)
+        self.board   = [ [Tile(x,y) for x in range(size)] for y in range(size) ]
+
+        for _ in range(num_mines):
+            self.random_empty().mine = True
+
+        for tile in self:
+            tile.number = sum( ntile.mine for ntile in self.neighbours(tile) )
+
+    def __getitem__(self, loc):
+        return self.board[loc.y][loc.x]
+
+    def __iter__(self):
+        """Iterate over board tile locations."""
+        return ( self[Loc(x,y)] for x in range(self.size) for y in range(self.size) )
+
+    def marked_or_revealed(self, tile):
+        return bool(not tile.hidden or tile.mine and tile.marked)
+
+    def cleared(self):
+        """All mines defused?"""
+        return all( self.marked_or_revealed(tile) for tile in self )
+
+    def all_hidden(self):
+        return [tile for tile in self if tile.hidden]
+
+    def random_hidden(self):
+        return rndchoice(self.all_hidden())
+
+    def all_empty(self):
+        return [tile for tile in self if not tile.mine]
+
+    def random_empty(self):
+        return rndchoice(self.all_empty())
+
+
+    def draw(self):
+        sp2 = space*2
+        print(space*4, sp2.join( [str(n+1) for n in range(self.size)] ))
+        print('\n')
+
+        for n, row in enumerate(self.board):
+            print(n+1, space, joins(row, space))
+            print()
+        print(self.divider)
+
+    def reveal(self, tile):
+        if not tile.number:
+            self.reveal_empty_neighbours(tile)
+        tile.hidden = False
+        return tile
+
+    def reveal_empty_neighbours(self, tile):
+        """ Reveal all empty (number=0) tiles adjacent to starting tile `loc` and subsequent unhidden tiles.
+            Uses floodfill algorithm.
+        """
+        if tile.number != 0:
+            tile.hidden = False
+        if not tile.hidden:
+            return
+
+        tile.hidden = False
+        for ntile in self.neighbours(tile):
+            self.reveal_empty_neighbours(ntile)
+
+    def neighbours(self, tile):
+        """Return the list of neighbours of `loc`."""
+        x, y = tile.loc
+        coords = (-1,0,1)
+        locs = set((x+n, y+m) for n in coords for m in coords) - set( [(x,y)] )
+        return [ self[ Loc(*tup) ] for tup in locs if self.valid(*tup) ]
+
+    def valid(self, x, y):
+        return bool( x+1 <= self.size and y+1 <= self.size and x >= 0 and y >= 0 )
+
+
+class Minesweeper(object):
+    start    = time()
+    win_msg  = "All mines cleared (%d:%d)!"
+    lose_msg = "KABOOM. END."
+
+    def __init__(self, board):
+        self.board = board
+
+    def check_end(self, tile):
+        """Check if game is lost (stepped on a mine), or won (all mines found)."""
+        if tile.mine and not tile.marked:
+            self.game_lost()
+        elif self.board.cleared():
+            self.game_won()
+
+    def game_lost(self):
+        board = self.board
+        for tile in board.all_hidden():
+            board.reveal(tile)
+
+        board.draw()
+        print(self.lose_msg)
+        sys.exit()
+
+    def game_won(self):
+        elapsed = time() - self.start
+        print(self.win_msg % (elapsed/60, elapsed%60))
+        sys.exit()
