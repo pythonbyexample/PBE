8634f345218eaba7fa0f3afeceb6d22cf1f070f6
M.Sirenef <msirenef@myserver>
2012-12-16 13:19:30 -0500

    small cleanup of rockpaper.py

--- code/rockpaper.py
+++ code/rockpaper.py
@@ -4,13 +4,11 @@
 import sys
 from random import choice as rndchoice
 from time import sleep
-from itertools import cycle
 
 players          = 'XY'
-player1, player2 = players
-ai_players       = 'Y'
+ai_players       = 'XY'
 moves            = "rps"
-wins             = ("rp", "sr", "ps")   # right choice wins
+wins             = ("rp", "sr", "ps")   # choice on the right side wins
 quit_key         = 'q'
 pause_time       = 0.3
 
@@ -26,23 +24,24 @@ class RockPaperScissors(object):
             choice1, choice2 = (self.get_move(player) for player in players)
 
             if choice1 != choice2:
-                win = int(choice1+choice2 in wins)
-                scores[win] += 1
-            print(self.status % (player1, scores[0], player2, scores[1]))
+                winplayer = 1 if choice1+choice2 in wins else 0
+                scores[winplayer] += 1
+            print(self.status % (players[0], scores[0], players[1], scores[1]))
             sleep(pause_time)
 
     def get_move(self, player):
         return self.ai_move() if player in ai_players else self.player_move()
 
     def ai_move(self):
-        return rndchoice("rps")
+        return rndchoice(moves)
 
     def player_move(self):
         while True:
             inp = input(self.prompt)
             if inp == quit_key : sys.exit()
-            if inp in moves    : return inp
+            elif inp in moves  : return inp
             else               : print(self.inv_move)
 
 if __name__ == "__main__":
-    RockPaperScissors().run()
+    try                      : RockPaperScissors().run()
+    except KeyboardInterrupt : pass
