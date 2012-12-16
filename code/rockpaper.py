3cfbd8bd3ac0896a7df87e37e58caa9f3dd111e2
M.Sirenef <msirenef@myserver>
2012-12-16 13:39:44 -0500

    ..

--- code/rockpaper.py
+++ code/rockpaper.py
@@ -5,12 +5,12 @@ import sys
 from random import choice as rndchoice
 from time import sleep
 
-players          = 'XY'
-ai_players       = 'XY'
-moves            = "rps"
-wins             = ("rp", "sr", "ps")   # choice on the right side wins
-quit_key         = 'q'
-pause_time       = 0.3
+players    = 'XY'
+ai_players = 'Y'
+moves      = "rps"
+wins       = ("rp", "sr", "ps")   # choice on the right side wins
+quit_key   = 'q'
+pause_time = 0.3
 
 
 class RockPaperScissors(object):
@@ -20,12 +20,14 @@ class RockPaperScissors(object):
 
     def run(self):
         scores = [0, 0]
+
         while True:
             choice1, choice2 = (self.get_move(player) for player in players)
 
             if choice1 != choice2:
                 winplayer = 1 if choice1+choice2 in wins else 0
                 scores[winplayer] += 1
+
             print(self.status % (players[0], scores[0], players[1], scores[1]))
             sleep(pause_time)
 
@@ -41,6 +43,7 @@ class RockPaperScissors(object):
             if inp == quit_key : sys.exit()
             elif inp in moves  : return inp
             else               : print(self.inv_move)
+
 
 if __name__ == "__main__":
     try                      : RockPaperScissors().run()
