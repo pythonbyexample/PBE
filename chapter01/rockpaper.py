38edf6a6473bc218a85dad824b8f0018ca0dbcdb
AK <andrei.avk@gmail.com>
2012-12-16 23:43:03 -0500

    updated rockpaper.py

--- chapter01/rockpaper.py
+++ chapter01/rockpaper.py
@@ -2,47 +2,40 @@
 # -*- encoding: utf-8 -*-
 
 import sys
-from random import choice as rndchoice
+from random import choice as randchoice
 from time import sleep
-from itertools import cycle
 
-players          = 'XY'
-player1, player2 = players
-ai_players       = 'Y'
-moves            = "rps"
-wins             = ("rp", "sr", "ps")   # right choice wins
-quit_key         = 'q'
-pause_time       = 0.3
+from utils import TextInput
+
+players    = 'XY'
+ai_players = 'Y'
+moves      = "rps"
+wins       = ("rp", "sr", "ps")   # choice on the right side wins
+status     = "%5s %3d %5s %3d     moves: %s %s"
+pause_time = 0.3
 
 
 class RockPaperScissors(object):
-    prompt   = "> "
-    inv_move = "Invalid move"
-    status   = "%5s %3d %5s %3d"
 
     def run(self):
-        scores = [0, 0]
+        self.textinput = TextInput("(r|p|s)")
+        scores         = [0, 0]
+
         while True:
-            choice1, choice2 = (self.get_move(player) for player in players)
+            choice1, choice2 = (self.get_move(p) for p in players)
 
             if choice1 != choice2:
-                win = int(choice1+choice2 in wins)
-                scores[win] += 1
-            print(self.status % (player1, scores[0], player2, scores[1]))
+                winplayer = 1 if (choice1+choice2 in wins) else 0
+                scores[winplayer] += 1
+
+            print(status % (players[0], scores[0], players[1], scores[1], choice1, choice2))
             sleep(pause_time)
 
     def get_move(self, player):
-        return self.ai_move() if player in ai_players else self.player_move()
+        if player in ai_players : return randchoice(moves)
+        else                    : return self.textinput.getval()
 
-    def ai_move(self):
-        return rndchoice("rps")
-
-    def player_move(self):
-        while True:
-            inp = input(self.prompt)
-            if inp == quit_key : sys.exit()
-            if inp in moves    : return inp
-            else               : print(self.inv_move)
 
 if __name__ == "__main__":
-    RockPaperScissors().run()
+    try                      : RockPaperScissors().run()
+    except KeyboardInterrupt : pass
