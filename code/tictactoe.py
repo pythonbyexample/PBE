13f134260e5384349b8ae950c5032181f5009cc4
M.Sirenef <msirenef@myserver>
2013-01-03 00:27:07 -0500

    updated tictactoe

--- code/tictactoe.py
+++ code/tictactoe.py
@@ -2,7 +2,7 @@
 # -*- encoding: utf-8 -*-
 
 import sys
-from random import choice as rndchoice
+from random import choice as randchoice
 from itertools import cycle
 
 from board import Loc, Board
@@ -17,7 +17,7 @@ class TictactoeBoard(Board):
         return not any(tile==blank for tile in self)
 
     def random_blank(self):
-        return rndchoice( [loc for loc in self.locations() if self[loc] == blank] )
+        return randchoice( [loc for loc in self.locations() if self[loc] == blank] )
 
     def completed(self, line, item):
         return all(self[loc] == item for loc in line)
@@ -29,16 +29,16 @@ class Tictactoe(object):
 
     def make_win_lines(self):
         """Create a list of winning lines -- when a player fills any one of them, he wins."""
-        lines, diag1, diag2 = [], [], []
+        winlines, diag1, diag2 = [], [], []
 
         for n in range(size):
-            lines.append( [Loc(m, n) for m in range(size)] )
-            lines.append( [Loc(n, m) for m in range(size)] )
+            winlines.append( [Loc(m, n) for m in range(size)] )
+            winlines.append( [Loc(n, m) for m in range(size)] )
 
             diag1.append(Loc(n, n))
-            diag2.append(Loc(2-n, n))
+            diag2.append(Loc(size-n-1, n))
 
-        return lines + [diag1, diag2]
+        return winlines + [diag1, diag2]
 
     def check_end(self, player):
         """Check if `player` has won the game; check for a draw."""
