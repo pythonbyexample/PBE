0d62810c7ef43ad5964c3ee894be058ae84f9755
AK <andrei.avk@gmail.com>
2013-01-08 17:55:01 -0500

    added menu() to TextInput; updated tmovies

--- chapter01/tmovies.py
+++ chapter01/tmovies.py
@@ -5,55 +5,75 @@ import os, sys
 import re
 from time import sleep
 
-from utils import TextInput, enumerate1, range1, sjoin, nl, first
+from utils import TextInput, BufferedIterator, getitem, iround, enumerate1, range1, sjoin, nl, first, space
 
 
-tut_dir   = "t-movies"
-screensep = 25
+tut_dir      = "t-movies"
+screensep    = 25
+cmdpat       = "(:sleep ?\d*|:clear)"
+stat_tpl     = " %s   %s"
+resetchar    = '\r'
+update_speed = 8        # for progress bar
 
-
-class MInput(TextInput):
-    choice_tpl = "%2d) %s"
-
-    def menu(self, choices):
-        self.choices = choices
-        for c in enumerate1(choices):
-            print(self.choice_tpl % c)
-
-        fmt = "(%s)" % sjoin( range1(len(choices)), '|' )
-        self.formats = [fmt]
-        return int(self.getval()) - 1
-
-# for x in range(5): print(MInput().menu("abcdefgabcdefgab"))
+# for x in range(5): print(TextInput().menu("abcdefgabcdefgab"))
+# nnoremap \s ^2daWk$xkk$xjddkP<<JJJ
+# nnoremap \S ^/if<cr>hs<cr><esc>/else<cr>hs<cr><esc>ea:<cr><esc>kkA:<esc>kddp>>2yaWjjP
 
 
 class Tutorial(object):
     def __init__(self, fn):
         with open(os.path.join(tut_dir, fn)) as fp:
-            self.sections = re.split("(:sleep ?\d*|:clear)", fp.read())
+            self.sections = re.split(cmdpat, fp.read())
 
     def play(self):
-        for section in self.sections:
-            if section.startswith(":sleep"):
-                sleep(int( section.strip().split()[1] ))
-            elif section == ":clear":
-                print(nl*screensep)
+        for section_num, section in enumerate1(self.sections):
+
+            if re.match(cmdpat, section):
+                section  = section.split()
+                cmd, arg = section[0], getitem(section, 1)
+
+                if   cmd == ":sleep": self.sleep(int(arg), section_num)
+                elif cmd == ":clear": print(nl * screensep)
+
             else:
-                print(section)
+                self.display(section)
+
+        print(nl*2, '-'*5, "END", '-'*5, nl*2)
+
+    def sleep(self, seconds, section_num):
+        for n in range(seconds * update_speed):
+            self.progress(section_num, n/update_speed, seconds)
+            sys.stdout.flush()
+            sleep(1 / update_speed)
+            print(resetchar + space*78 + resetchar, end='')
+
+    def display(self, section):
+        for line in section.split(nl):
+            print(space + line)
+            sleep(0.3)
+
+    def progress(self, section_num, n, total):
+        sprogress = progress_bar(len(self.sections), section_num, 45)
+        print( stat_tpl % (progress_bar(total, n, 20), sprogress), end='' )
 
 
 class TutorialMovies(object):
     def run(self):
-        inp = MInput()
-        mfiles = [fn for fn in os.listdir(tut_dir) if not fn.startswith('.')]
-        print("mfiles", mfiles)
+        inp     = TextInput()
+        mfiles  = [fn for fn in os.listdir(tut_dir) if not fn.startswith('.')]
         choices = [first(f.split('.')) for f in mfiles]
-        i = inp.menu(choices)
-        Tutorial(mfiles[i]).play()
+
+        while True:
+            i = inp.menu(choices)
+            Tutorial(mfiles[i]).play()
+
+
+def progress_bar(total, value, size=78, char='∘'):
+    size = size - 2
+    tpl  = "|%%-%ds|" % size
+    return tpl % (char * iround(size * value/total))
 
 
 if __name__ == "__main__":
-    try:
-        TutorialMovies().run()
-    except KeyboardInterrupt:
-        pass
+    try                      : TutorialMovies().run()
+    except KeyboardInterrupt : pass
